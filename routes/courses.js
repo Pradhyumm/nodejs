efb7e72214c9753aef4e6be60ffac33ff9846d63
Pradhyumm <pradhyump763@gmail.com>
2023-10-18 08:22:18 -0400

    added getall api

--- routes/courses.js
+++ routes/courses.js
@@ -4,7 +4,16 @@ const Course = require("../models/course")
 const router = ex.Router();
 
 
- 
+router.get("/", async (req,res)=>{
+    try{
+    const courses =await Course.find();
+    res.json(courses);
+    }
+    catch(err)
+    {
+        res.json(err);
+    }
+});
 
 
 module.exports = router;
